97fb403e5e0fb4331cf82ea146ba43743e4d1779
Jeyan Karthikeyan <jeyansaran620@gmail.com>
2025-04-29 16:28:16 +0000

    Update dist folder contents

---
+++ jeyansaran620.github.io/precache-manifest.4fe5e3b2b9255f9270f1cca0c42dd139.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2202116315bfcdbe639263be77e989",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34dab0bff93055af0a08",
+    "url": "/static/css/2.393d7f3f.chunk.css"
+  },
+  {
+    "revision": "af22247f6b2b3679449e",
+    "url": "/static/css/main.dfb3b242.chunk.css"
+  },
+  {
+    "revision": "34dab0bff93055af0a08",
+    "url": "/static/js/2.b4319a50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b4319a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af22247f6b2b3679449e",
+    "url": "/static/js/main.ddf5a7b3.chunk.js"
+  },
+  {
+    "revision": "84d0cbfa3e45af059ff1",
+    "url": "/static/js/runtime-main.e106fd5e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
